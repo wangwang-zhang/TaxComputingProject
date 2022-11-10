df191ff54419bdfb076e8556ec47672f53d9a351
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-10 17:03:40 +0800

    Refactored test function Should_Return_Correct_Tax_With_One_Month_Of_Salary

--- TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
+++ TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
@@ -7,17 +7,14 @@ public class TaxComputingServiceTest
     private readonly ITaxComputingService _taxComputingService = new TaxComputingServiceImpl();
 
     [Theory]
-    [InlineData(new Double[]{3000, 6000, 15000, 30000}, 1)]
-    public void Should_Return_Correct_Tax_With_One_Month_Of_Salary(Double[] salaries, int month)
+    [InlineData(3000, 1, 0)]
+    [InlineData(6000, 1, 30)]
+    [InlineData(15000, 1, 300)]
+    [InlineData(30000, 1, 750)]
+    public void Should_Return_Correct_Tax_With_One_Month_Of_Salary(Double salary, int month, double tax)
     {
-        var ResultOfCaseOne = _taxComputingService.ComputeTaxBySalaryAndMonth(salaries[0], month);
-        var ResultOfCaseTwo = _taxComputingService.ComputeTaxBySalaryAndMonth(salaries[1], month);
-        var ResultOfCaseThree = _taxComputingService.ComputeTaxBySalaryAndMonth(salaries[2], month);
-        var ResultOfCaseFour = _taxComputingService.ComputeTaxBySalaryAndMonth(salaries[3], month);
-        Assert.Equal(0, ResultOfCaseOne);
-        Assert.Equal(30, ResultOfCaseTwo);
-        Assert.Equal(300, ResultOfCaseThree);
-        Assert.Equal(750, ResultOfCaseFour);
+        var result = _taxComputingService.ComputeTaxBySalaryAndMonth(salary, month);
+        Assert.Equal(tax, result);
     }
 
     [Theory]
