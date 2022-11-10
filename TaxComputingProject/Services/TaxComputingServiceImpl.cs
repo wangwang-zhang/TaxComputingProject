6735e7d69dbb7bc322a9a6bc0d96252f8d33c4b8
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-10 16:58:47 +0800

    Implemented a service function to match tax rate by salary

--- TaxComputingProject/Services/TaxComputingServiceImpl.cs
+++ TaxComputingProject/Services/TaxComputingServiceImpl.cs
@@ -9,4 +9,18 @@ public class TaxComputingServiceImpl : ITaxComputingService
         double tax = (salary * month - 5000 * month) * 0.03;
         return tax;
     }
+
+    public double MatchTaxRateByTotalSalaryRemovedSalaryThreshold(double salary)
+    {
+        return salary switch
+        {
+            <= 36000 => 0.03,
+            > 36000 and <= 144000 => 0.1,
+            > 144000 and <= 300000 => 0.2,
+            > 300000 and <= 420000 => 0.25,
+            > 420000 and <= 660000 => 0.3,
+            > 660000 and <= 960000 => 0.35,
+            _ => 0.45
+        };
+    }
 }
--- TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
+++ TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
@@ -19,4 +19,19 @@ public class TaxComputingServiceTest
         Assert.Equal(300, ResultOfCaseThree);
         Assert.Equal(750, ResultOfCaseFour);
     }
+
+    [Theory]
+    [InlineData(30000, 0.03)]
+    [InlineData(45000, 0.1)]
+    [InlineData(220000, 0.2)]
+    [InlineData(350000, 0.25)]
+    [InlineData(500000, 0.3)]
+    [InlineData(800000, 0.35)]
+    [InlineData(1000000, 0.45)]
+    public void Should_Return_Correct_Tax_Rate_By_Total_Salary_Removed_Threshold(double salary, double taxRate)
+    {
+        var result = _taxComputingService.MatchTaxRateByTotalSalaryRemovedSalaryThreshold(salary);
+        Assert.Equal(taxRate, result);
+        
+    }
 }
