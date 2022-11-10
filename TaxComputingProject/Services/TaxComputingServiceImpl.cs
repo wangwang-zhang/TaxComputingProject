517131bf3859f3b650ad1482dd2bc7d07dcb0735
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-10 23:36:40 +0800

    Extract enum and struct

--- TaxComputingProject/Services/TaxComputingServiceImpl.cs
+++ TaxComputingProject/Services/TaxComputingServiceImpl.cs
@@ -15,13 +15,34 @@ public class TaxComputingServiceImpl : ITaxComputingService
     {
         return salary switch
         {
-            <= 36000 => 0.03,
-            > 36000 and <= 144000 => 0.1,
-            > 144000 and <= 300000 => 0.2,
-            > 300000 and <= 420000 => 0.25,
-            > 420000 and <= 660000 => 0.3,
-            > 660000 and <= 960000 => 0.35,
-            _ => 0.45
+            <= (double)TotalSalary.FirstLevel => TaxRates.FirstLevel,
+            > (double)TotalSalary.FirstLevel and <= (double)TotalSalary.SecondLevel => TaxRates.SecondLevel,
+            > (double)TotalSalary.SecondLevel and <= (double)TotalSalary.ThirdLevel => TaxRates.ThirdLevel,
+            > (double)TotalSalary.ThirdLevel and <= (double)TotalSalary.FourthLevel => TaxRates.FourthLevel,
+            > (double)TotalSalary.FourthLevel and <= (double)TotalSalary.FifthLevel => TaxRates.FifthLevel,
+            > (double)TotalSalary.FifthLevel and <= (double)TotalSalary.SixthLevel => TaxRates.SixthLevel,
+            _ => TaxRates.SeventhLevel
         };
+    }
+
+    private enum TotalSalary
+    {
+        FirstLevel = 36000,
+        SecondLevel = 144000,
+        ThirdLevel = 300000,
+        FourthLevel = 420000,
+        FifthLevel = 660000,
+        SixthLevel = 960000
+    }
+
+    struct TaxRates
+    {
+         public const double FirstLevel = 0.03;
+         public const double SecondLevel = 0.1;
+         public const double ThirdLevel = 0.2;
+         public const double FourthLevel = 0.25;
+         public const double FifthLevel = 0.3;
+         public const double SixthLevel = 0.35;
+         public const double SeventhLevel = 0.45;
     }
 }
