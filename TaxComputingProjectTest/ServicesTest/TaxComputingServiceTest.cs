6a1235ce423895ed067a0d5feca0e01e67c50fef
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-11 15:57:08 +0800

    Changed test data to match boundary point

--- TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
+++ TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
@@ -7,10 +7,9 @@ public class TaxComputingServiceTest
     private readonly ITaxComputingService _taxComputingService = new TaxComputingServiceImpl();
 
     [Theory]
-    [InlineData(3000, 1, 0)]
-    [InlineData(6000, 1, 30)]
-    [InlineData(15000, 1, 300)]
-    [InlineData(30000, 1, 750)]
+    [InlineData(5000, 1, 0)]
+    [InlineData(36000, 1, 930)]
+    [InlineData(41000, 1, 1080)]
     public void Should_Return_Correct_Tax_With_One_Month_Of_Salary(Double salary, int month, double tax)
     {
         var result = _taxComputingService.ComputeTaxBySalaryAndMonth(salary, month);
@@ -18,12 +17,12 @@ public class TaxComputingServiceTest
     }
 
     [Theory]
-    [InlineData(30000, 0.03)]
-    [InlineData(45000, 0.1)]
-    [InlineData(220000, 0.2)]
-    [InlineData(350000, 0.25)]
-    [InlineData(500000, 0.3)]
-    [InlineData(800000, 0.35)]
+    [InlineData(36000, 0.03)]
+    [InlineData(144000, 0.1)]
+    [InlineData(300000, 0.2)]
+    [InlineData(420000, 0.25)]
+    [InlineData(660000, 0.3)]
+    [InlineData(960000, 0.35)]
     [InlineData(1000000, 0.45)]
     public void Should_Return_Correct_Tax_Rate_By_Total_Salary_Removed_Threshold(double salary, double taxRate)
     {
