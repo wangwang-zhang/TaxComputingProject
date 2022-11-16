ebfd0a38202ed526d3280a4cdac8aa69fccb8be4
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-16 16:35:34 +0800

    Added a request model UserLoginRequest

---
+++ TaxComputingProject/Model/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbTest.Models
+{
+    public class UserLoginRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
