de955f1ef60f2855e8935ca38bf19114fb0811fc
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-20 18:29:05 +0800

    Refactored to extract common function

--- TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
+++ TaxComputingProjectTest/ServicesTest/TaxComputingServiceTest.cs
@@ -1,11 +1,12 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Moq;
 using TaxComputingProject.Services;
 
 namespace TaxComputingProjectTest.ServicesTest;
 
 public class TaxComputingServiceTest
 {
-    private readonly ITaxComputingService _taxComputingService = new TaxComputingServiceImpl();
-
     [Theory]
     [InlineData(new double[]{5000}, 0)]
     [InlineData(new double[]{36000},930)]
@@ -18,7 +19,8 @@ public class TaxComputingServiceTest
     [InlineData(new double[]{1000000},265830)]
     public void Should_Return_Correct_Tax_With_One_Month_Of_Salary(double[] salary, double tax)
     {
-        var result = _taxComputingService.ComputeTaxBySalaryAndMonth(salary, 1);
+        var taxComputingService = MockService();
+        var result = taxComputingService.ComputeTaxBySalaryAndMonth(salary, 1);
         Assert.Equal(tax, result);
     }
 
@@ -32,7 +34,8 @@ public class TaxComputingServiceTest
     [InlineData(1000000, 0.45, 181920)]
     public void Should_Return_Correct_Tax_Rate_And_Deduction_By_Taxable_Salary(double salary, double taxRate, double deduction)
     {
-        var result = _taxComputingService.MatchTaxRateAndDeductionBySalary(salary);
+        var taxComputingService = MockService();
+        var result = taxComputingService.MatchTaxRateAndDeductionBySalary(salary);
         Assert.Equal(taxRate, result.TaxRate);
         Assert.Equal(deduction,result.Deduction);
     }
@@ -47,7 +50,17 @@ public class TaxComputingServiceTest
     [InlineData(new double[]{41000, 113000, 161000, 125000, 245000, 305000, 45000}, 7, 18000)]
     public void Should_Return_Correct_Tax_With_Multiple_Month_Of_Salary(double[] salary, int month, double tax)
     {
-        var result = _taxComputingService.ComputeTaxBySalaryAndMonth(salary, month);
+        var taxComputingService = MockService();
+        var result = taxComputingService.ComputeTaxBySalaryAndMonth(salary, month);
         Assert.Equal(tax, result);
+    }
+    
+    private static ITaxComputingService MockService()
+    {
+        var accessorMock = new Mock<IHttpContextAccessor>();
+        var context = new DefaultHttpContext();
+        accessorMock.Setup(a => a.HttpContext).Returns(context);
+        ITaxComputingService taxComputingService = new TaxComputingServiceImpl(accessorMock.Object);
+        return taxComputingService;
     }
 }
