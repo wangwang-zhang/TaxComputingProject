6654537ffeff252550a21ad8f23108fa78c6e196
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-28 14:09:27 +0800

    Added test for function GetUserId in controller

--- TaxComputingProjectTest/ControllerTest/TaxComputingControllerTest.cs
+++ TaxComputingProjectTest/ControllerTest/TaxComputingControllerTest.cs
@@ -16,4 +16,14 @@ public class TaxComputingControllerTest
         var result = taxComputingController.GetMonthOfTax(It.IsIn(1, 12));
         Assert.IsType<OkObjectResult>(result);
     }
+
+    [Fact]
+    public void Should_Return_Ok_When_Get_UserId_Correctly()
+    {
+        var mockService = new Mock<ITaxComputingService>();
+        mockService.Setup(user => user.GetId()).Returns(It.IsAny<int>());
+        var taxComputingController = new TaxComputingController(mockService.Object);
+        var result = taxComputingController.GetUserId();
+        Assert.IsType<OkObjectResult>(result.Result);
+    }
 }
