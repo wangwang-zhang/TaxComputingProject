324c77f718b04d83b32b0c62958a7d9d5b5e55ff
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-11-29 21:29:27 +0800

    refactor

--- TaxComputingProjectTest/ControllerTest/TaxComputingControllerTest.cs
+++ TaxComputingProjectTest/ControllerTest/TaxComputingControllerTest.cs
@@ -8,16 +8,6 @@ namespace TaxComputingProjectTest.ControllerTest;
 public class TaxComputingControllerTest
 {
     [Fact]
-    public void Should_Return_Ok_When_Get_Tax_By_Month()
-    {
-        var mockService = new Mock<ITaxComputingService>();
-        mockService.Setup(user => user.GetTaxOfMonth(It.IsAny<int>())).Returns(It.IsAny<double>());
-        var taxComputingController = new TaxComputingController(mockService.Object);
-        var result = taxComputingController.GetMonthOfTax(It.IsIn(1, 12));
-        Assert.IsType<OkObjectResult>(result);
-    }
-
-    [Fact]
     public void Should_Return_Ok_When_Get_UserId_Correctly()
     {
         var mockService = new Mock<ITaxComputingService>();
