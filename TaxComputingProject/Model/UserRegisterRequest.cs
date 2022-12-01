fd50b6bc765a2c19c2f2e1e333c997a2d1879828
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-12-01 14:15:53 +0800

    feat: added phone number format verification

--- TaxComputingProject/Model/UserRegisterRequest.cs
+++ TaxComputingProject/Model/UserRegisterRequest.cs
@@ -8,7 +8,7 @@ namespace TaxComputingProject.Model
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
         
-        [Phone]
+        [RegularExpression("^((13[0-9])|(15[1-3,5-9])|(18[0-9]))\\d{8}$", ErrorMessage = "Can't identify phone number")]
         public string Phone { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string Job { get; set; } = string.Empty;
