fade8f6a1d010da5930196034c63968e2dd156d0
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-12-09 11:33:32 +0800

    refactor

--- TaxComputingProjectTest/DaoTest/UserDaoTest.cs
+++ TaxComputingProjectTest/DaoTest/UserDaoTest.cs
@@ -20,6 +20,31 @@ public class UserDaoTest
         _context = new DataContext(dbOptions.Options);
     }
 
+    private readonly User _testUser = new()
+    {
+        Email = "Hello@example.com",
+        Phone = "13812344321",
+        Job = "teacher",
+        Address = "Xi'an",
+        PasswordHash = new byte[32],
+        PasswordSalt = new byte[32],
+        VerificationToken = "",
+        VerifiedAt = null
+    };
+
+    private readonly UserTax _testUserTax = new()
+    {
+        UserId = 1,
+        Taxes = new List<TaxOfMonth>
+        {
+            new() { Id = 1, Month = 1, Salary = 41000, Tax = 1080 },
+            new() { Id = 2, Month = 2, Salary = 41000, Tax = 3600 },
+            new() { Id = 3, Month = 3, Salary = 41000, Tax = 3600 },
+            new() { Id = 4, Month = 4, Salary = 41000, Tax = 3600 },
+            new() { Id = 5, Month = 5, Salary = 41000, Tax = 7200 },
+        }
+    };
+
     [Fact]
     public void Should_Return_True_When_User_Exist()
     {
@@ -33,18 +58,7 @@ public class UserDaoTest
     public void Should_Return_Correct_Count_When_Add_One_User()
     {
         var userDao = new UserDaoImpl(_context);
-        User user = new User
-        {
-            Email = "Hello2@example.com",
-            Phone = "13812344321",
-            Job = "teacher",
-            Address = "Xi'an",
-            PasswordHash = new byte[32],
-            PasswordSalt = new byte[32],
-            VerificationToken = "",
-            VerifiedAt = null
-        };
-        userDao.AddUser(user);
+        userDao.AddUser(_testUser);
         Assert.Equal(1, _context.Users.Count());
     }
 
@@ -60,40 +74,16 @@ public class UserDaoTest
     [Fact]
     public void Should_Return_Correct_Count_When_Add_One_UserTax()
     {
-        UserTax userTax = new()
-        {
-            UserId = 1,
-            Taxes = new List<TaxOfMonth>()
-            {
-                new() { Id = 1, Month = 1, Salary = 41000, Tax = 1080 },
-                new() { Id = 2, Month = 2, Salary = 41000, Tax = 3600 },
-                new() { Id = 3, Month = 3, Salary = 41000, Tax = 3600 },
-                new() { Id = 4, Month = 4, Salary = 41000, Tax = 3600 },
-                new() { Id = 5, Month = 5, Salary = 41000, Tax = 7200 },
-            }
-        };
         var userDao = new UserDaoImpl(_context);
-        userDao.AddUserTax(userTax);
+        userDao.AddUserTax(_testUserTax);
         Assert.Equal(1, _context.UserTaxes.Count());
     }
 
     [Fact]
     public void Should_Return_Correct_UserTax_By_Id()
     {
-        UserTax testUserTax = new()
-        {
-            UserId = 1,
-            Taxes = new List<TaxOfMonth>()
-            {
-                new() { Id = 1, Month = 1, Salary = 41000, Tax = 1080 },
-                new() { Id = 2, Month = 2, Salary = 41000, Tax = 3600 },
-                new() { Id = 3, Month = 3, Salary = 41000, Tax = 3600 },
-                new() { Id = 4, Month = 4, Salary = 41000, Tax = 3600 },
-                new() { Id = 5, Month = 5, Salary = 41000, Tax = 7200 },
-            }
-        };
         var userDao = new UserDaoImpl(_context);
-        userDao.AddUserTax(testUserTax);
+        userDao.AddUserTax(_testUserTax);
         UserTax? userTax = userDao.GetUserTaxById(1);
         Assert.Equal(5, userTax?.Taxes.Count);
     }
@@ -101,19 +91,10 @@ public class UserDaoTest
     [Fact]
     public void Should_Return_Zero_When_Remove_Tax_Items_By_Id()
     {
-        UserTax testUserTax = new()
-        {
-            UserId = 1,
-            Taxes = new List<TaxOfMonth>()
-            {
-                new() { Id = 1, Month = 1, Salary = 41000, Tax = 1080 },
-                new() { Id = 2, Month = 2, Salary = 41000, Tax = 3600 },
-            }
-        };
         var userDao = new UserDaoImpl(_context);
-        userDao.AddUserTax(testUserTax);
+        userDao.AddUserTax(_testUserTax);
         userDao.RemoveTaxItem(1);
-        UserTax? userTax = userDao.GetUserTaxById(1);
+        var userTax = userDao.GetUserTaxById(1);
         if (userTax != null) Assert.Empty(userTax.Taxes);
     }
 
@@ -121,19 +102,8 @@ public class UserDaoTest
     public void Should_Return_Non_Empty_User_When_Find_User_By_Updated_Info()
     {
         UserDaoImpl userDao = new UserDaoImpl(_context);
-        User testUser = new User
-        {
-            Email = "Hello@example.com",
-            Phone = "13812344321",
-            Job = "teacher",
-            Address = "Xi'an",
-            PasswordHash = new byte[32],
-            PasswordSalt = new byte[32],
-            VerificationToken = "",
-            VerifiedAt = null
-        };
-        userDao.AddUser(testUser);
-        UserInfo userInfo = new UserInfo()
+        userDao.AddUser(_testUser);
+        var userInfo = new UserInfo
         {
             Email = "test@example.com",
             Address = "New York",
@@ -153,19 +123,8 @@ public class UserDaoTest
     public void Should_Still_Return_Non_Empty_User_When_Find_User_By_Previous_Email()
     {
         UserDaoImpl userDao = new UserDaoImpl(_context);
-        User testUser = new User
-        {
-            Email = "Hello@example.com",
-            Phone = "13812344321",
-            Job = "teacher",
-            Address = "Xi'an",
-            PasswordHash = new byte[32],
-            PasswordSalt = new byte[32],
-            VerificationToken = "",
-            VerifiedAt = null
-        };
-        userDao.AddUser(testUser);
-        UserInfo userInfo = new UserInfo()
+        userDao.AddUser(_testUser);
+        var userInfo = new UserInfo
         {
             Address = "New York",
             Job = "doctor",
@@ -173,7 +132,7 @@ public class UserDaoTest
         };
         userDao.UpdateUserInfo(1, userInfo);
         var result = _context.Users
-            .Where(user => user.Email == testUser.Email)
+            .Where(user => user.Email == _testUser.Email)
             .Where(user => user.Address == userInfo.Address)
             .Where(user => user.Job == userInfo.Job)
             .FirstOrDefault(user => user.Phone == userInfo.Phone);
@@ -184,19 +143,8 @@ public class UserDaoTest
     public void Should_Return_User_When_Find_User_By_Id_Successfully()
     {
         UserDaoImpl userDao = new UserDaoImpl(_context);
-        User testUser = new User
-        {
-            Email = "Hello@example.com",
-            Phone = "13812344321",
-            Job = "teacher",
-            Address = "Xi'an",
-            PasswordHash = new byte[32],
-            PasswordSalt = new byte[32],
-            VerificationToken = "",
-            VerifiedAt = null
-        };
-        userDao.AddUser(testUser);
-        User? user = userDao.GetUserById(1);
+        userDao.AddUser(_testUser);
+        var user = userDao.GetUserById(1);
         Assert.NotNull(user);
     }
 
